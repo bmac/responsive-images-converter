93c9b4ef5a80071262862ba8978564744630ee76
bmac <bmac325@gmail.com>
2016-06-30 15:48:38 -0400

    Add a path config option

--- tests/index-test.js
+++ tests/index-test.js
@@ -31,10 +31,10 @@ describe('ResponsiveImageConverter', function() {
   });
 
 
-  it('generateSrcset should resolve with sizes src and srcset', function () {
-    const subject = createSubjectForTest({'foo': 'bar'});
+  it('resizeImage should resolve with sizes src and srcset', function () {
+    const subject = createSubjectForTest();
 
-    return subject.generateSrcset('file-path.jpg', {})
+    return subject.resizeImage('file-path.jpg', {})
       .then(attributes => {
         assert.equal(attributes.sizes, '100vw');
         assert.equal(attributes.src, 'file-path.jpg');
@@ -42,19 +42,29 @@ describe('ResponsiveImageConverter', function() {
       });
   });
 
-  it('generateSrcset should generate sizes based on the config', function () {
+  it('resizeImage should generate sizes based on the config', function () {
     const subject = createSubjectForTest({
       breakpoints: {
         small: '(min-width: 768px)',
       }
     });
 
-    return subject.generateSrcset('file-path.jpg', { small: '100vw', default: '50vw'})
+    return subject.resizeImage('file-path.jpg', { small: '100vw', default: '50vw'})
       .then(attributes => {
         assert.equal(attributes.sizes, '(min-width: 768px) 100vw, 50vw');
 
         assert.equal(attributes.src, 'file-path.jpg');
         assert.equal(attributes.srcset, 'file-path-200.jpg 200w, file-path-340.jpg 340w, file-path-480.jpg 480w, file-path-620.jpg 620w, file-path-760.jpg 760w, file-path-900.jpg 900w, file-path-1040.jpg 1040w, file-path-1180.jpg 1180w, file-path-1320.jpg 1320w, file-path-1460.jpg 1460w');
+      });
+  });
+
+  it('should prefix the image with the path', function () {
+    const subject = createSubjectForTest({ path: 'https://www.example.com/static/' });
+
+    return subject.resizeImage('file-path.jpg', {})
+      .then(attributes => {
+        assert.equal(attributes.src, 'https://www.example.com/static/file-path.jpg');
+        assert.equal(attributes.srcset, 'https://www.example.com/static/file-path-200.jpg 200w, https://www.example.com/static/file-path-340.jpg 340w, https://www.example.com/static/file-path-480.jpg 480w, https://www.example.com/static/file-path-620.jpg 620w, https://www.example.com/static/file-path-760.jpg 760w, https://www.example.com/static/file-path-900.jpg 900w, https://www.example.com/static/file-path-1040.jpg 1040w, https://www.example.com/static/file-path-1180.jpg 1180w, https://www.example.com/static/file-path-1320.jpg 1320w, https://www.example.com/static/file-path-1460.jpg 1460w');
       });
   });
 });
